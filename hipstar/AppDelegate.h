1d9e577ba4d2c77fe621eb9f6488cc10db817ad8
hellozimi <hello@zimi.se>
2012-11-09 16:57:35 +0100

    initial commit

---
+++ hipstar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hipstar
+//
+//  Created by Simon Andersson on 11/9/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hipstar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hipstar
+//
+//  Created by Simon Andersson on 11/9/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

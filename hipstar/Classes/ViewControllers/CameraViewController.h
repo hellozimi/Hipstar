f73ce8abb65f13b9caf2869accb18c30e2670a29
hellozimi <hello@zimi.se>
2012-11-09 17:04:58 +0100

    added resources

---
+++ hipstar/Classes/ViewControllers/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  hipstar
+//
+//  Created by Simon Andersson on 11/9/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end

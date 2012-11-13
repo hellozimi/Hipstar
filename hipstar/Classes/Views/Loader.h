d31269a40128c2c61587de2653c2c4c586b6f6d1
hellozimi <hello@zimi.se>
2012-11-14 00:47:27 +0100

    Added loader

---
+++ hipstar/Classes/Views/Loader.h
@@ -0,0 +1,13 @@
+//
+//  Loader.h
+//  hipstar
+//
+//  Created by Simon Andersson on 11/13/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Loader : UIWindow
+
+@end
---
+++ hipstar/Classes/Views/Loader.m
@@ -0,0 +1,31 @@
+//
+//  Loader.m
+//  hipstar
+//
+//  Created by Simon Andersson on 11/13/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import "Loader.h"
+
+@implementation Loader
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

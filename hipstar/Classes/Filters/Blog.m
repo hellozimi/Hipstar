362a747bcbea5ff991b3a5966463a34eb0af1043
hellozimi <hello@zimi.se>
2012-11-15 02:02:18 +0100

    added blog filter

--- hipstar/Classes/Filters/Blog.m
+++ hipstar/Classes/Filters/Blog.m
@@ -10,4 +10,37 @@
 
 @implementation Blog
 
+- (UIImage *)apply:(UIImage *)image {
+    
+    CGContextRef context = BitmapContextCreateWithImage(image, image.size);
+    float scale = [UIScreen mainScreen].scale;
+    CGRect rect = CGRectMake(0, 0, image.size.width*scale, image.size.height*scale);
+    
+    float offset = rect.size.width * 0.076;
+    
+    CGContextSaveGState(context);
+    
+    CGContextSetFillColorWithColor(context, [[UIColor whiteColor] colorWithAlphaComponent:0.11].CGColor);
+    CGContextSetBlendMode(context, kCGBlendModeScreen);
+    CGContextFillRect(context, rect);
+    CGContextRestoreGState(context);
+    
+    CGContextSaveGState(context);
+    CGContextDrawImage(context, CGRectInset(rect, offset, offset), image.CGImage);
+    CGContextRestoreGState(context);
+    image = BitmapImageCreateFromContext(context);
+    
+    CGContextRelease(context);
+    
+    return image;
+}
+
+- (NSString *)name {
+    return @"Blog";
+}
+
+- (UIImage *)image {
+    return [UIImage imageNamed:@"Blog"];
+}
+
 @end

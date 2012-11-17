3be25388c86d88ae19a04fe8cabb957cf6cc1b27
hellozimi <hello@zimi.se>
2012-11-17 10:40:52 +0100

    new jug filter

--- hipstar/Classes/Filters/Jug.m
+++ hipstar/Classes/Filters/Jug.m
@@ -11,7 +11,7 @@
 @implementation Jug
 
 - (UIImage *)apply:(UIImage *)image {
-    
+    /*
     UIImage *jug = [UIImage imageNamed:@"jug_filter"];
     
     GPUImagePicture *bottom = [[GPUImagePicture alloc] initWithImage:image];
@@ -23,8 +23,22 @@
     
     [bottom processImage];
     [top processImage];
+     
+     return [muliply imageFromCurrentlyProcessedOutput];
+    */
     
-    return [muliply imageFromCurrentlyProcessedOutput];
+    image = [self applyLookup:@"jug_lookup_filter_1" image:image];
+    
+    image = [self imageWithOverlayBlendingBottom:image top:[UIImage imageNamed:@"jug_filter"]];
+    
+    UIImage *overlayImage = [self imageName:@"full_gradient.jpg" alpha:0.5];
+    image = [self imageWithOverlayBlendingBottom:image top:overlayImage];
+    
+    UIImage *vignette = [self imageName:@"vignette" alpha:0.3];
+    
+    image = [self imageWithOverlayBlendingBottom:image top:vignette];
+    
+    return image;
 }
 
 - (NSString *)name {
